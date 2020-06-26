811c5d0fd16382168dedf110afd17bbe92a11b7f
Shridhar Sukhani <shridhar.sukhani@yahoo.in>
2020-06-26 02:09:15 -0700

    Open TaskListItemComponent for a dummy task in LoginActivity

--- app/src/main/java/com/shrisukhani/cleantodo/LoginActivity.kt
+++ app/src/main/java/com/shrisukhani/cleantodo/LoginActivity.kt
@@ -2,11 +2,32 @@ package com.shrisukhani.cleantodo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.facebook.litho.ComponentContext
+import com.facebook.litho.LithoView
+import com.shrisukhani.cleantodo.components.TaskListItemComponent
+import com.shrisukhani.cleantodo.components.TaskListItemComponent2
+import com.shrisukhani.cleantodo.components.TaskListItemComponentSpec
+import com.shrisukhani.cleantodo.model.Task
+import com.shrisukhani.cleantodo.model.TaskPriority
+import java.time.LocalDate
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        val componentContext = ComponentContext(this)
+        val component = TaskListItemComponent
+            .create(componentContext)
+            .task(getDummyTask())
+            .build()
+
+        setContentView(LithoView.create(componentContext, component))
     }
+
+    private fun getDummyTask() = Task(
+        title = "Physics Midterm Prep",
+        priority = TaskPriority.HIGH,
+        dueDate = LocalDate.of(2021, 6, 25)
+    )
 }
