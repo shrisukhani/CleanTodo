33589de81a737837b871082870523dab8411ff92
Shridhar Sukhani <shridhar.sukhani@yahoo.in>
2020-06-28 22:56:41 -0700

    Introduce basic structure for TaskDetailDescriptionEditorComponent

--- app/src/main/java/com/shrisukhani/cleantodo/components/TaskDetailDescriptionEditorComponentSpec.kt
+++ app/src/main/java/com/shrisukhani/cleantodo/components/TaskDetailDescriptionEditorComponentSpec.kt
@@ -1,4 +1,32 @@
 package com.shrisukhani.cleantodo.components
 
+import com.facebook.litho.Component
+import com.facebook.litho.ComponentContext
+import com.facebook.litho.annotations.LayoutSpec
+import com.facebook.litho.annotations.OnCreateLayout
+import com.facebook.litho.annotations.Prop
+import com.facebook.litho.widget.TextInput
+import com.facebook.litho.widget.VerticalScroll
+import com.shrisukhani.cleantodo.model.Task
+
+@LayoutSpec
 class TaskDetailDescriptionEditorComponentSpec {
+    companion object {
+        private val FADING_EDGE_LENGTH_DP = 50f
+
+        @JvmStatic
+        @OnCreateLayout
+        fun onCreateLayout(c: ComponentContext, @Prop task: Task): Component = VerticalScroll.create(c)
+            .childComponent(
+                TextInput.create(c)
+                    .editable(true)
+                    .initialText(task.description)
+                    .multiline(true)
+                    .heightPercent(100f)
+                    .widthPercent(100f))
+            .flex(1f)
+            .verticalFadingEdgeEnabled(true)
+            .fadingEdgeLengthDip(FADING_EDGE_LENGTH_DP)
+            .build()
+    }
 }
